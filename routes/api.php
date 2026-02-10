9139123612eaefd69d92082f4caf7ee98fe4c020
Martin-Rouault <martin.rlt24@gmail.com>
2026-02-10 12:00:36 +0100

    add match method for put & patch methods

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
 
     Route::post('/books', [BookController::class, 'store']);
-    Route::put('/books/{book}', [BookController::class, 'update']);
-    Route::patch('/books/{book}', [BookController::class, 'update']);
-    Route::delete('/books/{book}', [BookController::class, 'destroy']); 
+    Route::match(['put, patch'], 'books/{book}', [BookController::class, 'update']);
+    Route::delete('/books/{book}', [BookController::class, 'destroy']);
 });
-
-Route::apiResource('books', BookController::class);
