e642459e9b5c32dfca45afd5c9094005dc01b288
Martin-Rouault <martin.rlt24@gmail.com>
2026-02-10 09:58:39 +0100

    activité 2

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return BookResource::collection(Book::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:3|max:255',
+            'author' => 'required|min:3|max:100',
+            'summary' => 'required|min:10|max:500',
+            'isbn' => 'required|size:13|unique:books,isbn'
+        ]);
+
+        $book = Book::create($request->all());
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        return new BookResource($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required|min:3|max:255',
+            'author' => 'required|min:3|max:100',
+            'summary' => 'required|min:10|max:500',
+            'isbn' => [
+                'required',
+                'size:13',
+                Rule::unique('books')->ignore($book->id),
+            ],
+        ]);
+
+        $book->update($request->all());
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book): JsonResponse
+    {
+        $book->delete();
+
+        return response()->json(null, 204);
+    }
+}
